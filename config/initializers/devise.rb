7660f911a6c5dc1714dc9dcadc279169babd8bb5
Mike Stubna <mike@mikembp.local>
2015-02-13 11:09:04 -0500

    try again

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,6 +2,11 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
 
+ # The secret key used by Devise. Devise uses this key to generate
+  # random tokens. Changing this key will render invalid all existing
+  # confirmation, reset password and unlock tokens in the database.
+  config.secret_key = Rails.application.secrets.secret_key_base
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
